23ce687e39c146933268ef05dff61b50f71f3040
feri muhibudin <38092606+ferimuhib@users.noreply.github.com>
2026-05-27 17:40:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
 echo "=== Pulling latest update ==="
 git -C ~/vfx pull
-
+chmod +x ~/vfx/update.sh
 echo "=== Running install.sh ==="
 bash ~/vfx/doinstall.sh
-
 echo "=== Update selesai! ==="
